570803a801fb5bf41f563634714b0fceb18254c5
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-09-14 11:21:48 +0900

    java4章

--- Chapter02.java
+++ Chapter02.java
@@ -3,6 +3,14 @@ public class Chapter02 {
 	public static void main(String... args) {
 		System.out.println("Hello WEBCAMP!");
 		System.out.println("Hello Java!");
+
+		String name = "谷口";
+		System.out.println(name);
+		name = "こんにちは";
+		System.out.println(name);
+
+		final int NUMBER = 7;
+		System.out.println(NUMBER);
 	}
 
 }
