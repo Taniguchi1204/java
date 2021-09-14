2d13e661c277b3a50715ddc4e069b5a4cc22aeb9
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-09-14 11:47:37 +0900

    java5章

---
+++ Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long number1 = 100L;
+		double number2 = 0.5;
+		boolean a = true;
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(a);
+		
+		Integer i = 100;
+		Float f = 0.526f;
+		Character c = 'あ';
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(c);
+		
+		int[] array = {0,1,2,3,4};
+		System.out.println(array[3]);
+	}
+
+}
